be81d2d6331aa5f35698f84bd5eafee8b1f7fa3b
Aman Zadran <amanzadran9@gmail.com>
2020-11-03 13:15:40 +0100

    feat: all features implemented.

--- index.js
+++ index.js
@@ -1,1 +1,104 @@
-console.log("HEJJ");
+let hasLoanedOnce = false;
+// set first laptop as the initially selected one.
+let selectedLaptop = store[0];
+let payBalanceEl = document.getElementById("pay-balance");
+let bankBalanceEl = document.getElementById("bank-balance");
+let laptopPriceEl = document.getElementById("laptop-price");
+
+updateLaptopInfo();
+
+function getBankBalance() {
+  return Number(bankBalanceEl.innerText.split(" ")[0]);
+}
+
+function getPayBalance() {
+  return Number(payBalanceEl.innerText.split(" ")[0]);
+}
+
+document.getElementById("btn-bank").addEventListener("click", () => {
+  bankBalanceEl.innerText = getBankBalance() + getPayBalance() + " Kr.";
+  payBalanceEl.innerText = 0 + " Kr.";
+});
+
+document.getElementById("btn-work").addEventListener("click", () => {
+  payBalanceEl.innerText = getPayBalance() + 100 + " Kr.";
+});
+
+document.getElementById("btn-get-loan").addEventListener("click", () => {
+  if (hasLoanedOnce) {
+    alert("You can only loan once before a purchase..");
+    return;
+  }
+
+  let amountToLoan = prompt(
+    "Please enter the amount you want to loan",
+    getBankBalance()
+  );
+
+  amountToLoan = Number(amountToLoan);
+
+  if (isNaN(amountToLoan)) {
+    alert("Only numbers are allowed. Try again!");
+    return;
+  }
+
+  if (amountToLoan <= getBankBalance() * 2) {
+    bankBalanceEl.innerText = getBankBalance() + amountToLoan + " Kr.";
+    hasLoanedOnce = true;
+  } else if (getBankBalance() === 0) {
+    alert(
+      "Your bank balance seems to 0. You need to earn by working before we can lend you money."
+    );
+  } else {
+    alert("You can't loan more than double of your existing balance..");
+  }
+});
+document.getElementById("btn-buy-now").addEventListener("click", () => {
+  // We check if we have enough balance to complete the purchase
+  if (getBankBalance() >= selectedLaptop.price) {
+    // we check if we have already bought this laptop. If we have, we
+    // want to confirm from the user that he/she really wants to buy the same one again.
+    // If not, we just return and stop the execution. Otherwise we continue.
+    if (selectedLaptop.bought) {
+      const continueBuy = confirm(
+        "You have already bought this computer. Do you still wish to continue?"
+      );
+
+      if (!continueBuy) {
+        return;
+      }
+    }
+
+    bankBalanceEl.innerText = getBankBalance() - selectedLaptop.price;
+
+    alert("You bought a new " + selectedLaptop.name + " computer!");
+
+    selectedLaptop.bought = true;
+    hasLoanedOnce = false;
+  } else {
+    alert(
+      "You don't have enough money in your bank account to make the purchase... "
+    );
+  }
+});
+
+document.getElementById("laptops").addEventListener("change", (event) => {
+  const selectedLaptopId = event.target.value;
+
+  selectedLaptop = store.find(
+    (laptop) => laptop.id === Number(selectedLaptopId)
+  );
+
+  updateLaptopInfo();
+});
+
+function updateLaptopInfo() {
+  document.getElementById("features-output").innerText =
+    selectedLaptop.features;
+  document.getElementById("laptop-name").innerText = selectedLaptop.name;
+  document.getElementById("laptop-description").innerText =
+    selectedLaptop.description;
+  document.getElementById("laptop-price").innerText =
+    selectedLaptop.price + " Kr.";
+  document.getElementById("laptop-image").src = selectedLaptop.image;
+}
---
+++ store.js
@@ -0,0 +1,44 @@
+const store = [
+  {
+    id: 1,
+    price: 3000,
+    name: "Hp notebook",
+    features:
+      "HP Laptop is a thin (1.99 cm), lightweight (1.78 kg) and powerful laptop with elegant design.",
+    description:
+      "Slim, reliable laptop with a large screen Stay connected for what matters most with long battery life.",
+    image: "./images/hplatop.png",
+  },
+  {
+    id: 2,
+    price: 3500,
+    name: "Surface",
+    features:
+      "Surface Laptop  is the result of efforts to develop the ideal portable productivity device. It weighs only 1.5 kg and is less than 1.4 cm thick.",
+    description:
+      "Surface Laptop  is the result of efforts to develop the ideal portable productivity device. It weighs only 1.5 kg and is less than 1.4 cm thick, but enhances performance.",
+    image: "./images/surface.png",
+  },
+  {
+    id: 3,
+    price: 4000,
+    name: "Dell",
+    features:
+      "15.6-inch performance laptop with a stunning OLED display option. Now featuring 9th Gen Intel® Core™ processors.",
+    description:
+      "Dell Laptop is a Windows 10 Home laptop with a 15.60-inch display that has a resolution of 1920x1080 pixels. It is powered by a Core i5 processor and it comes with 8GB of RAM.",
+    image: "./images/dell.png",
+  },
+  {
+    id: 4,
+    price: 5000,
+    name: "Lenovo",
+    features:
+      "the Lenovo is a true masterpiece for gamers. Take your gaming experience to the next level with the Lenovo",
+    description:
+      "It's time for a new gaming look. With a thickness of 20 mm and a weight of only 2.2 kg, wrapped in premium aluminum, our Lenovo is built to break the limits of gaming laptops.",
+    image: "./images/lenovo.png",
+  },
+];
+
+// Ta bort package.json node modules osv ... skapa ny branch!(utveckling)
